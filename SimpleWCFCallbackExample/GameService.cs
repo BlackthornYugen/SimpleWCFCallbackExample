b583de12a007fd554372b73d8aa4bcc1e20b14cb
John Steel <john@steelcomputers.com>
2015-11-11 21:11:08 -0500

    Refactored into a number guessing game.

---
+++ SimpleWCFCallbackExample/GameService.cs
@@ -0,0 +1,13 @@
+﻿using System.ServiceModel;
+
+namespace SimpleWCFCallbackExample
+{
+    [ServiceBehavior]
+    public class GameService : IGameService
+    {
+        public string GuessNumber(int value)
+        {
+            return string.Format("You guessed: {0}\nWaiting for other players to guess...", value);
+        }
+    }
+}
---
+++ SimpleWCFCallbackExample/IGameService.cs
@@ -0,0 +1,24 @@
+﻿using System.ServiceModel;
+
+namespace SimpleWCFCallbackExample
+{
+    [ServiceContract]
+    public interface IGameService
+    {
+        [OperationContract]
+        string GuessNumber(int value);
+    }
+
+    [ServiceContract(CallbackContract = typeof(IBroadcastorCallback))]
+    public interface IGameCallbacks
+    {
+        [OperationContract(IsOneWay = true)]
+        void RegisterClient(string clientName);
+    }
+
+    public interface IBroadcastorCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void BroadcastToClient(string message);
+    }
+}
